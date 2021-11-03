7eb8197221e7d3b5321b2b6f18688c8277d99130
Vinay Matta <vinaymatta63@gmail.com>
2021-11-03 16:47:43 +0530

    check2

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -2,6 +2,8 @@ import api from "../axios";
 
 export const loginUser = async (data) => {
   try {
+    //
+    //
     const response = await api.post("/auth/signin", data, {
       headers: { "Content-Type": "application/json" },
     });
